ce86657573f04f57a6bac4b7644fb16890ff4b3e
DESKTOP <ilya.kamckine@ya.ru>
2022-05-21 19:25:43 +0300

    files

--- obu.py
+++ obu.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 import functions_gen_layers as fgl
 import matplotlib.image as img
 
-CUDA = True
-device = torch.device('cuda')
+CUDA, LOAD = False, True
+device = torch.device('cuda' if CUDA else 'cpu')
 layers = fgl.gen_image(nn.Sigmoid, 4, 100, True)
 
 
@@ -20,6 +20,7 @@ class NN(nn.Module):
     def __init__(self, layers: tuple[nn.Sequential, str]):
         super(NN, self).__init__()
         self.layers, self.metadata = layers
+        self.flatten = nn.Flatten()
         if CUDA:
             self.layers = self.layers.to(device=device, non_blocking=True)
 
@@ -28,13 +29,8 @@ class NN(nn.Module):
 
 
 def fit(model, data, batch_size=100, train=True):
-    # важно для Dropout, BatchNorm
     model.train(train)
-    # ошибка, точность, батчей
     for xb, yb in data.items():
-        xb, yb = torch.Tensor(xb), torch.Tensor(yb)
-        if CUDA:
-            xb, yb = xb.to(device=device, non_blocking=True), yb.to(device=device, non_blocking=True)
         # прямое распространение
         y = model(xb)
         L = loss(y, yb)  # вычисляем ошибку
@@ -45,19 +41,18 @@ def fit(model, data, batch_size=100, train=True):
             optimizer.step()  # подправляем параметры
 
 
-def map_point(point, size):
-    return torch.Tensor(list(map(lambda x: 2 * x / size - 1, point)))
-
-
-def show(image):
+def show(image, is_save: bool = False):
     plt.imshow(image)
+    if is_save:
+        plt.savefig(f'images_gen/{datetime.datetime.now():%Y-%m-%d--%H-%M-%S}.jpg')
     plt.show()
 
 
 def gen_data_lern(data, size):
     to_neuro = lambda x, y: (2 * x / size[0] - 1, 2 * y / size[1] - 1)
     return {
-        to_neuro(x, y): data[x][y]
+        torch.Tensor(to_neuro(x, y)).to(device=device, non_blocking=True):
+            torch.Tensor(data[x][y]).to(device=device, non_blocking=True)
         for x in range(size[0]) for y in range(size[1])
     }
 
@@ -77,46 +72,49 @@ def get_image_from_neuro(neuro, size):
     return data
 
 
-def save(epoch: int):
-    state = {'info': f"эпоха: {epoch}",  # описание
-             'date': datetime.datetime.now(),  # дата и время
+def save(epoch: int, lern_time):
+    state = {'epoch': epoch,  # описание
+             'lern_time': lern_time,
              'model': model.state_dict(),  # параметры модели
              'optimizer': optimizer.state_dict()}  # состояние оптимизатора
 
-    torch.save(state, 'state.pt')  # сохраняем файл
+    torch.save(state, 'state.pth')  # сохраняем файл
 
 
 def load():
-    state = torch.load('state.pt')
-    model = NN(layers).load_state_dict(state['model'])
-    optimizer = torch.optim.SGD(model.parameters(), lr=1).load_state_dict(state['optimizer'])
-    print(state['info'], state['date'])  # вспомогательная информация
-    return model, optimizer
+    state = torch.load('state.pth')
+    model = NN(layers).to(device=device, non_blocking=True)
+    model.load_state_dict(state['model'])
+    optimizer = torch.optim.SGD(model.parameters(), lr=1)
+    optimizer.load_state_dict(state['optimizer'])
+    print(f'epoch={state["epoch"]} lern_time={state["lern_time"]}')  # вспомогательная информация
+    return model, optimizer, state['lern_time'], state["epoch"]
 
 
 if __name__ == '__main__':
-    if CUDA:
-        model = NN(layers).to(device=device, non_blocking=True).apply(init_normal)
-    else:
-        model = NN(layers).apply(init_normal)
-    print(model.metadata)
-
-    start = datetime.datetime.now()
+    model = NN(layers).to(device=device, non_blocking=True).apply(init_normal)
     loss = nn.MSELoss().to(device=device, non_blocking=True)
     optimizer = torch.optim.SGD(model.parameters(), lr=0.002, momentum=0.9, nesterov=True)
     Y = img.imread('images/image.jpg') / 255
     SIZE = Y.shape
     #
-    epochs = 1000  # число эпох
+    start_epoch, epochs = 0, 10000  # число эпох
     data = gen_data_lern(Y, Y.shape)
-    for epoch in range(epochs):  # эпоха - проход по всем примерам
+    start = datetime.datetime.now()
+    last = datetime.datetime.now()
+
+    if LOAD:
+        model, optimizer, l_time, start_epoch = load()
+        start -= l_time
+
+    for epoch in range(start_epoch, epochs):
+        now = datetime.datetime.now()
+        print(f'\tвремени прошло: {now - start} (+{now - last})')
+        last = datetime.datetime.now()
         fit(model, data)  # одна эпоха
-        print(f'времени прошло: {datetime.datetime.now() - start}')
-
-        if epoch % 100 == 0:
+        if not epoch % 5:
+            show(get_image_from_neuro(model, SIZE), is_save=True)
+        if not epoch % 100:
             print(f'{epoch=}')
-            show(get_image_from_neuro(model, SIZE))
-            # save(epoch)
-
-#    print(model(torch.Tensor(X[32][32])))
-
+            # show(get_image_from_neuro(model, SIZE))
+            save(epoch, now - start)
